f94c9ecc46797d0da5fac9ca3cc29e0a9f73462d
soham <sohampatel9403@gmail.com>
2022-03-21 15:35:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-
+import robot from './robot.glb'
+import modal2 from './modal2.glb'
+import poster from './page.png'
+import { useEffect, useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [animationList, setAnimationList] = useState([])
+  const [currentAnimation, setCurrentAnimation] = useState('')
+
+  const [currentAnmationIndex, setCurrentAnimationIndex] = useState(0)
+
+  useEffect(() => {
+    const modalViewer = document.querySelector('model-viewer#animation_demo')
+
+    modalViewer.addEventListener('load', function () {
+
+      if (modalViewer.availableAnimations && modalViewer.availableAnimations.length > 0) {
+        setAnimationList(modalViewer.availableAnimations)
+      }
+    })
+  }, [])
+
+  
+
+  const changeAnimation = (animation_name) => {
+    setCurrentAnimation(animation_name)
+  }
+  const setANimation = (index) => {
+    if (animationList.length > 0) { setCurrentAnimationIndex((currentAnmationIndex + 1) % animationList.length) }
+  }
+
+  useEffect(()=>{
+    changeAnimation(animationList[currentAnmationIndex])
+  },)
+
+return (
+  <div className="App">
+    <div style={{}} >
+      {/* dropdown */}
+      {/* {animationList && animationList.length > 0 &&
+          <select name="animation" onChange={(e) => { changeAnimation(e.target.value) }} id="">
+            {animationList.map((data, index) => {
+              return <option value={data} key={index}>{data}</option>
+            })}
+          </select>} */}
+      <model-viewer id="animation_demo" orientation="0deg 0deg 0deg" style={{ innerWidth: "100%" }} loading="eager" autoplay ar shadow-intensity="1" animation-name={currentAnimation} camera-controls poster={poster} src={robot} alt="A 3D model of a shishkebab">
+        <button className='modal-btn' onClick={setANimation}>Change Animation</button>
+        <span className='modal-text' >Animation : {currentAnimation}</span>
+      </model-viewer>
     </div>
-  );
+
+  </div>
+);
 }
 
 export default App;
