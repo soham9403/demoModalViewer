b3d66807cd2e80366085554152298958488dca6b
soham <sohampatel9403@gmail.com>
2022-03-21 15:58:36 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     })
   }, [])
 
-  
+
 
   const changeAnimation = (animation_name) => {
     setCurrentAnimation(animation_name)
@@ -30,28 +30,35 @@ function App() {
     if (animationList.length > 0) { setCurrentAnimationIndex((currentAnmationIndex + 1) % animationList.length) }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     changeAnimation(animationList[currentAnmationIndex])
-  },)
+  })
 
-return (
-  <div className="App">
-    <div style={{}} >
-      {/* dropdown */}
-      {/* {animationList && animationList.length > 0 &&
+  return (
+    <div className="App">
+      <div style={{}} >
+        {/* dropdown */}
+        {/* {animationList && animationList.length > 0 &&
           <select name="animation" onChange={(e) => { changeAnimation(e.target.value) }} id="">
             {animationList.map((data, index) => {
               return <option value={data} key={index}>{data}</option>
             })}
           </select>} */}
-      <model-viewer id="animation_demo" orientation="0deg 0deg 0deg" style={{ innerWidth: "100%" }} loading="eager" autoplay ar shadow-intensity="1" animation-name={currentAnimation} camera-controls poster={poster} src={robot} alt="A 3D model of a shishkebab">
+        {/* <model-viewer id="animation_demo" orientation="0deg 0deg 0deg" style={{ innerWidth: "100%" }} loading="eager" autoplay ar shadow-intensity="1" animation-name={currentAnimation} camera-controls poster={poster} src={robot} alt="A 3D model of a shishkebab">
         <button className='modal-btn' onClick={setANimation}>Change Animation</button>
         <span className='modal-text' >Animation : {currentAnimation}</span>
-      </model-viewer>
-    </div>
+      </model-viewer> */}
+        <model-viewer ar ar-modes="webxr scene-viewer quick-look" camera-controls
+          src={robot}
+          alt="A 3D model of an astronaut">
+          <button slot="ar-button" style="background-color: white; border-radius: 4px; border: none; position: absolute; top: 16px; right: 16px; ">
+            👋 Activate AR
+          </button>
+        </model-viewer>
+      </div>
 
-  </div>
-);
+    </div>
+  );
 }
 
 export default App;
